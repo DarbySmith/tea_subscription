f31e9d9e0a63afc9483149db56ed69220dc07ac9
Darby Smith <newman.darby2@gmail.com>
2023-03-01 16:41:59 -0500

    endpoint to subscribe customer to tea subscription

--- app/models/subscription_customer.rb
+++ app/models/subscription_customer.rb
@@ -5,4 +5,7 @@ class SubscriptionCustomer < ApplicationRecord
   has_many :teas, through: :subscription_teas
 
   validates_presence_of :status, :frequency
+
+  enum status: { active: 0, skipped: 1, cancelled: 2 }
+  enum frequency: { weekly: 0, bi_monthly: 1, monthly: 2 }
 end
